5e8aa477ba0baf5a31861eac2319741b1a70762b
echang1802 <41762304+echang1802@users.noreply.github.com>
2020-11-23 19:36:46 -0300

    agragado std

--- 6_ejercicio.py
+++ 6_ejercicio.py
@@ -22,14 +22,15 @@ custom = {
     'products': 6,  # RELLENAR
     'plants': 4,  # RELLENAR
     'capacities': [1000, 1500, 2000, 1800],  # RELLENAR
+                # [1100, 1800, 2500, 2500]
     'demands' : [500, 1000, 800, 1500, 600, 1900],
     'configuration': [
-        [1,0,0,0],
-        [0,1,0,0],
-        [0,0,1,0],
-        [0,0,0,1],
-        [1,1,0,0],
-        [0,0,1,1]
+        [1,1,0,0], # 500
+        [1,0,0,0], # 1000
+        [0,0,0,0], # 800
+        [0,0,0,0], # 1500
+        [0,0,0,0], # 600
+        [0,0,0,0]  # 1900
     ]  # RELLENAR
 }
 
@@ -39,16 +40,17 @@ def poisson_demand_sampler(instance):
 
 
 # Devuelve observaciones de la forma [[ventas_1, utilizacion_1], ..., [ventas_N, utilizacion_N]]
-#observations = simulate(flexibilidad_plena, poisson_demand_sampler, 10000)
+observations = simulate(flexibilidad_plena, poisson_demand_sampler, 10000)
 
 # zip(*[[1, 2], [3, 4]]) = [1, 3], [2, 4]
-#sales, utilizaciones = zip(*observations)
-#es_6300 = [observation > 6299 for observation in sales]
+sales, utilizaciones = zip(*observations)
+es_6300 = [observation > 6299 for observation in sales]
 
-#print('Flexibilidad plena')
-#print('Ventas: %.2f' % (sum(sales) / len(sales)))
-#print('Utilizacion: %.2f' % (sum(utilizaciones) / len(utilizaciones)))
-#print('P(Ventas=6300): %.2f' % (sum(es_6300) / len(es_6300)))
+print('Flexibilidad plena')
+print('Ventas: %.2f' % (sum(sales) / len(sales)))
+print('Ventas: %.2f' % (sum(sales) / len(sales)))
+print('Utilizacion: %.2f' % (sum(utilizaciones) / len(utilizaciones)))
+print('P(Ventas=6300): %.2f' % (sum(es_6300) / len(es_6300)))
 
 # Devuelve observaciones de la forma [[ventas_1, utilizacion_1], ..., [ventas_N, utilizacion_N]]
 observations = simulate(custom, poisson_demand_sampler, 10000)
@@ -60,5 +62,6 @@ es_6300 = [observation > 6299 for observation in sales]
 print('')
 print('Custom')
 print('Ventas: %.2f' % (sum(sales) / len(sales)))
+print('Ventas: %.2f' % np.std(sales))
 print('Utilizacion: %.2f' % (sum(utilizaciones) / len(utilizaciones)))
 print('P(Ventas=6300): %.2f' % (sum(es_6300) / len(es_6300)))

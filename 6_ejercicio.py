0215467b0e866898ee3a01804c25946a3a6aef6a
echang1802 <41762304+echang1802@users.noreply.github.com>
2020-11-23 19:40:30 -0300

    Correccion std

--- 6_ejercicio.py
+++ 6_ejercicio.py
@@ -25,12 +25,12 @@ custom = {
                 # [1100, 1800, 2500, 2500]
     'demands' : [500, 1000, 800, 1500, 600, 1900],
     'configuration': [
-        [1,1,0,0], # 500
-        [1,0,0,0], # 1000
-        [0,0,0,0], # 800
-        [0,0,0,0], # 1500
-        [0,0,0,0], # 600
-        [0,0,0,0]  # 1900
+         [1,0,0,0],
+         [0,1,0,0],
+         [0,0,1,0],
+         [0,0,0,1],
+         [1,1,1,0],
+         [0,1,1,1]
     ]  # RELLENAR
 }
 
@@ -48,7 +48,7 @@ es_6300 = [observation > 6299 for observation in sales]
 
 print('Flexibilidad plena')
 print('Ventas: %.2f' % (sum(sales) / len(sales)))
-print('Ventas: %.2f' % (sum(sales) / len(sales)))
+print('Ventas: %.2f' % np.std(sales))
 print('Utilizacion: %.2f' % (sum(utilizaciones) / len(utilizaciones)))
 print('P(Ventas=6300): %.2f' % (sum(es_6300) / len(es_6300)))
 
